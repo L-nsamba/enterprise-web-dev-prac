d628b5c6761a96c0e358ddb20adc8ea8f5135f84
L-nsamba <l.nsamba@alustudent.com>
2026-01-12 18:02:25 +0200

    more-oop

--- py4e/oop.py
+++ py4e/oop.py
@@ -19,11 +19,30 @@ class PartyAnimal:
 
     def party(self):
         self.x = self.x + 1
-        print(self.name, "party_count", self.x)
+        print(self.name, "Party count:", self.x)
 
-s = PartyAnimal("SAL")
+# s = PartyAnimal("SAL")
+# s.party()
+# s.party()
+
+# j = PartyAnimal("KIM")
+# j.party()
+
+class FootballFan(PartyAnimal):
+    def __init__(self, name):
+        super().__init__(name)
+        self.points = 0
+    
+    def touchdown(self):
+        self.points = self.points + 7
+        print(self.name, "Points Now:", self.points)
+
+s = PartyAnimal("Jill")
 s.party()
 s.party()
 
-j = PartyAnimal("KIM")
-j.party()
+n = FootballFan("Noah")
+n.touchdown()
+n.party()
+n.touchdown()
+        
